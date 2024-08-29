2f8c80ec1f4a2488ff7b549c53ebc6fb463964bc
Dams <rifflartdamien68@gmail.com>
2024-08-29 11:26:48 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex w-full h-screen justify-center items-center'>
+        <h1 className='text-[var(--accent)] text-5xl font-bold'>MétéoMatin</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
